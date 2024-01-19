6104e32982a034343f8d7b96777a2b9b018f6a8e
Dmitry <dmt.ivanov1@yandex.ru>
2024-01-19 20:49:58 +0300

    Настройка окружения

--- Makefile
+++ Makefile
@@ -1,20 +1,18 @@
 install: install-deps
 	npx simple-git-hooks
 
-run:
-	bin/nodejs-package.js 10
+develop:
+	npx webpack serve
 
-install-deps:
+install:
 	npm ci
+
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
 
 test:
 	npm test
 
-test-coverage:
-	npm test -- --coverage --coverageProvider=v8
-
 lint:
-	npx eslint .
-
-publish:
-	npm publish
+	npx eslint 
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
+
+export default () => {
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
