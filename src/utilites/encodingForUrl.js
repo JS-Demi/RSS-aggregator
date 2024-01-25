4fe22c78f3e1bae003f23112cc1c2f0275e21220
Dmitry <dmt.ivanov1@yandex.ru>
2024-01-25 18:34:29 +0300

    return first version encodingUrl

--- src/utilites/encodingForUrl.js
+++ src/utilites/encodingForUrl.js
@@ -1,7 +1,1 @@
-// export default (url) => (`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
-export default (link) => {
-  const url = new URL('https://allorigins.hexlet.app/get');
-  url.searchParams.set('disabledCache', true);
-  url.searchParams.set('url', link);
-  return url.toString();
-};
+export default (url) => (`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`);
